59ba07d8876b4492172dcbcdbfe2babaede1a5bf
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2019-07-09 03:17:11 +0800

    Add social media icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,5 @@
 $(window).scroll(function() {
   $scrollTop = $(document).scrollTop();
-  console.log($scrollTop);
-  // if ($scrollTop > 800) {
-  //     $('#nav-about').addClass('nav-link-scrolled');
-  //     $('nav').addClass('navbar-scrolled');
-  //     if ($scrollTop > 1525) {
-  //     $('#nav-about').removeClass('nav-link-scrolled');
-  //   } 
-  // } 
-  // else {
-  //     $('#nav-about').removeClass('nav-link-scrolled');
-  //     $('nav').removeClass('navbar-scrolled');
-  // } 
-
-  // if ($scrollTop > 1525) {
-  //     $('#nav-skills').addClass('nav-link-scrolled');
-  //     $('nav').addClass('navbar-scrolled');
-  //     if ($scrollTop > 2000) {
-  //     $('#nav-skills').removeClass('nav-link-scrolled');
-  //     $('nav').removeClass('navbar-scrolled');
-  //   } 
-  // } 
-    // else {
-    //     $('#nav-skills').removeClass('nav-link-scrolled');
-    //     $('nav').removeClass('navbar-scrolled');
-    // } 
-  
-
   if ($scrollTop > 800) {
       // $('#nav-skills').addClass('nav-link-scrolled');
       $('nav').addClass('navbar-scrolled');
@@ -50,3 +23,70 @@ $('.nav-link').on('click', function(e){
     )
   }
 })
+
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 50;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 100;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
